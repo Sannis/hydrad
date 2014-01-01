a2374f9383b781e32240f9c9b103944deacc7c2c
Oleg Efimov <efimovov@gmail.com>
2014-01-02 03:22:56 +0400

    Skeleton for simple TCP server

--- src/hydrad.c
+++ src/hydrad.c
@@ -1,15 +1,67 @@
 /* Hydra (c) 2013 Oleg Efimov */
 
 #include <stdio.h>
+#include <unistd.h>
 
 #include "util.h"
 #include "uv.h"
 
+#define HYDRAD_PORT 88888
+
+void signal_handler(uv_signal_t *handle, int signum)
+{
+  hlog_info("SIGINT received, exiting");
+  uv_signal_stop(handle);
+  exit(0);
+}
+
+void on_new_connection(uv_stream_t *server, int status) {
+  if (status == -1) {
+      return;
+  }
+
+  uv_loop_t *loop = uv_default_loop();
+
+  uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+  uv_tcp_init(loop, client);
+  //if (uv_accept(server, (uv_stream_t*) client) == 0) {
+  //    uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+  //} else {
+      uv_close((uv_handle_t*)client, NULL);
+  //}
+}
+
 int main()
 {
-  uv_loop_t *loop = uv_loop_new();
+  hlog_info("Starting...");
 
-  hlog_info("Now quitting");
+  uv_loop_t *loop = uv_default_loop();
+
+  hlog_info("Init signal handler...");
+  uv_signal_t signal;
+  uv_signal_init(loop, &signal);
+  uv_signal_start(&signal, signal_handler, SIGINT);
+
+  hlog_info("Init server...");
+  uv_tcp_t server;
+  uv_tcp_init(loop, &server);
+
+  struct sockaddr_in address;
+  if (uv_ip4_addr("0.0.0.0", HYDRAD_PORT, &address) != 0) {
+    hlog_error("Wrong socket address");
+    return 1;
+  }
+  if (uv_tcp_bind(&server, (struct sockaddr*)&address) != 0) {
+    hlog_error("Cannot bind server");
+    return 2;
+  }
+  if (uv_listen((uv_stream_t*) &server, 128, on_new_connection) != 0) {
+    hlog_error("Cannot start listening");
+    //TODO: uv_err_name(uv_last_error(loop)));
+    return 3;
+  }
+
+  hlog_info("Enter event loop");
   uv_run(loop, UV_RUN_DEFAULT);
 
   return 0;
