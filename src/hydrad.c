97f4b175b2109684d98c37f8b27d24af38768228
Oleg Efimov <efimovov@gmail.com>
2014-01-02 20:04:02 +0400

    Reorder functions in hydra.c

--- src/hydrad.c
+++ src/hydrad.c
@@ -14,63 +14,11 @@
 #define HYDRAD_EXIT_BIND    2
 #define HYDRAD_EXIT_LISTEN  3
 
-void signal_handler(uv_signal_t *handle, int signum)
-{
-  hlog_info("Signal %d received, exiting", signum);
-  uv_signal_stop(handle);
-  exit(HYDRAD_EXIT_SUCCESS);
-}
-
-void on_connection_close(uv_handle_t* handle)
-{
-  hlog_debug("Connection closed");
-}
-
-void on_read(uv_stream_t* tcp, ssize_t nread, const uv_buf_t* buf)
-{
-  hlog_debug("Read bytes: %d", nread);
-  if (nread >= 0) {
-    //
-  } else {
-    if (nread != UV_EOF) {
-      //UVERR(nread, "read");
-    }
-    uv_close((uv_handle_t*)tcp, on_connection_close);
-  }
-  free(buf->base);
-}
-
-void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t* buf)
-{
-    *buf = uv_buf_init((char*)malloc(suggested_size), suggested_size);
-}
-
-void on_new_connection(uv_stream_t *server, int status)
-{
-  if (status != 0) {
-    hlog_error("Cannot handle new connection: %s", uv_err_name(status));
-    return;
-  }
-
-  uv_loop_t *loop = uv_default_loop();
-  int err;
-
-  hlog_debug("New connection");
-
-  uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
-  uv_tcp_init(loop, client);
-  if ((err = uv_accept(server, (uv_stream_t*) client)) != 0) {
-    hlog_error("Cannot accept connection: %s", uv_err_name(err));
-    uv_close((uv_handle_t*)client, NULL);
-    return;
-  }
-
-  if ((err = uv_read_start((uv_stream_t*)client, alloc_buffer, on_read)) != 0) {
-    hlog_error("Cannot start reading data from client: %s", uv_err_name(err));
-    uv_close((uv_handle_t*)client, NULL);
-    return;
-  }
-}
+void signal_handler(uv_signal_t *handle, int signum);
+void on_new_connection(uv_stream_t *server, int status);
+void on_read(uv_stream_t* tcp, ssize_t nread, const uv_buf_t* buf);
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t* buf);
+void on_connection_close(uv_handle_t* handle);
 
 int main()
 {
@@ -106,4 +54,62 @@ int main()
   uv_run(loop, UV_RUN_DEFAULT);
 
   return HYDRAD_EXIT_SUCCESS;
+}
+
+void signal_handler(uv_signal_t *handle, int signum)
+{
+  hlog_info("Signal %d received, exiting", signum);
+  uv_signal_stop(handle);
+  exit(HYDRAD_EXIT_SUCCESS);
+}
+
+void on_new_connection(uv_stream_t *server, int status)
+{
+  if (status != 0) {
+    hlog_error("Cannot handle new connection: %s", uv_err_name(status));
+    return;
+  }
+
+  uv_loop_t *loop = uv_default_loop();
+  int err;
+
+  hlog_debug("New connection");
+
+  uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+  uv_tcp_init(loop, client);
+  if ((err = uv_accept(server, (uv_stream_t*) client)) != 0) {
+    hlog_error("Cannot accept connection: %s", uv_err_name(err));
+    uv_close((uv_handle_t*)client, NULL);
+    return;
+  }
+
+  if ((err = uv_read_start((uv_stream_t*)client, alloc_buffer, on_read)) != 0) {
+    hlog_error("Cannot start reading data from client: %s", uv_err_name(err));
+    uv_close((uv_handle_t*)client, NULL);
+    return;
+  }
+}
+
+void on_read(uv_stream_t* tcp, ssize_t nread, const uv_buf_t* buf)
+{
+  hlog_debug("Read bytes: %d", nread);
+  if (nread >= 0) {
+    //
+  } else {
+    if (nread != UV_EOF) {
+      //UVERR(nread, "read");
+    }
+    uv_close((uv_handle_t*)tcp, on_connection_close);
+  }
+  free(buf->base);
+}
+
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t* buf)
+{
+    *buf = uv_buf_init((char*)malloc(suggested_size), suggested_size);
+}
+
+void on_connection_close(uv_handle_t* handle)
+{
+  hlog_debug("Connection closed");
 }
