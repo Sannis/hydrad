8fbe5439e8a77cf0cc0be73efd8bbf2b6fc59a6f
Oleg Efimov <efimovov@gmail.com>
2014-01-26 21:54:09 +0100

    Fix test

--- tests/method_version.py
+++ tests/method_version.py
@@ -4,7 +4,6 @@ from base import HydraTestCase, daemon_version, uv_version
 class HydraVersionMethodTestCase(HydraTestCase):
     def test_version(self):
         response = self.makeRequest('version', {})
-        self.assertEqual(response['data'], {'version': daemon_version})
 
         # Check versions
         self.assertEqual(response['data']['version'], daemon_version)
